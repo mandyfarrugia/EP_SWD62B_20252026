e548fc4a1f764891bba7d0fabe364a0125de7295
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-10-09 10:56:13 +0200

    Added the necessary NuGet packages to enable migrations.

---
+++ EP_SWD62B_20252026/Presentation/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+using System.Diagnostics;
+
+namespace Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult BasicContentResult()
+        {
+            return new ContentResult() { Content = "Hello from BasicContentResult", ContentType = "text/plain" };
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
